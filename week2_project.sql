5c3a24beda7084a4ff664376c64216e73fa3d3fa
Robert Ochi <c4f117@gmail.com>
2021-03-27 00:27:39 -0600

    more files
    extra commit

--- week2_project.sql
+++ week2_project.sql
@@ -7,3 +7,21 @@ SELECT  t.title,
     GROUP BY t.title
     ORDER BY count(*) DESC
     limit 200;
+
+SELECT t.title, AVG(s.salary)
+    FROM titles t
+    INNER JOIN salaries s
+    ON t.emp_no = s.emp_no
+    GROUP BY t.title
+    ORDER BY count(*) DESC
+    limit 200;
+
+
+SELECT d.dept_no, SUM(s.salary)
+    FROM dept_emp d
+    INNER JOIN salaries s
+    ON d.emp_no = s.emp_no
+    WHERE s.from_date > '1990-01-01' AND s.to_date < '1992-01-01' AND d.dept_no = 'd001'
+    GROUP BY d.dept_no
+    ORDER BY count(*) DESC
+    limit 200;
